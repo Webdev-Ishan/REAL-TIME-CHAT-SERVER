a9c11504d2fc599a27b9a1d903f098e8cf22843b
Ishan Saini <ishansaini0105@gmail.com>
2025-05-20 11:49:07 +0530

    The backedn logic is created

---
+++ Backend/Models/messages.model.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const messageSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    required: true,
+  },
+  from: {
+    type: Object,
+    required: true,
+  },
+  to: {
+    type: String,
+    required: true,
+  },
+  Socketid: {
+    type: String,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: String,
+    required: true,
+  },
+});
+
+const messageModel = new mongoose.model("Message", messageSchema);
+
+export default messageModel;
--- Backend/app.js
+++ Backend/app.js
@@ -6,16 +6,19 @@ import DbConnect from "./Config/mongoDb.js";
 import cors from "cors";
 import authRouter from "./Routes/auth.Routes.js";
 import cloudConfig from "./Config/cloudinary.js";
-
+import User from "./Models/user.Model.js";
+import messageModel from "./Models/messages.model.js";
+import http from "http";
+import { Server } from "socket.io";
 
 const port = process.env.PORT || 3000;
 
 const app = express();
-
+const server = http.createServer(app);
+const io = new Server(server);
 
 DbConnect();
 cloudConfig();
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -29,7 +32,62 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 
+// to find the messages by the room and fetch thme according to date
+const getlastmessages = async (room) => {
+  let messages = await messageModel.aggregate([
+    { $match: { to: room } },
+    { $group: { _id: "$date", messagesByDate: { $push: "$$ROOT" } } },
+  ]);
 
-app.listen(port, () => {
+  return messages;
+};
+
+// arrnge them according to the date
+function sortRoomMessagesByDate(messages) {
+  return messages.sort(function (a, b) {
+    let date1 = a._id.split("/");
+    let date2 = b._id.split("/");
+
+    date1 = date1[2] + date1[0] + date1[1];
+    date2 = date2[2] + date2[0] + date2[1];
+
+    return date1 < date2 ? -1 : 1;
+  });
+}
+
+io.on("connection", (socket) => {
+  socket.on("new-user", async () => {
+    let members = await User.find({});
+    io.emit("new-member", members);
+  });
+
+  socket.on("join-room", async (newroom, previousroom) => {
+    socket.join(newroom);
+    socket.leave(previousroom);
+    let previouschat = await getlastmessages(newroom);
+    let roomMessages = sortRoomMessagesByDate(previouschat);
+    socket.emit("room-messages", roomMessages);
+  });
+
+  socket.on("message-room", async (room, sender, time, date, content) => {
+    let newMessage = await messageModel.create({
+      from: sender,
+      time,
+      date,
+      content,
+      to: room,
+    });
+    let previouschat = await getlastmessages(room);
+    let roomMessages = sortRoomMessagesByDate(previouschat);
+
+    io.to(room).emit("room-messages", roomMessages);
+    socket.broadcast.emit("notifications", room);
+  });
+});
+
+
+
+
+server.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
